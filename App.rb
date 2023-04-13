42fec80151cd67bdc77bae1f88826d16e93e3ba5
Kmike <44137594+kmike-wachira@users.noreply.github.com>
2023-04-13 07:39:17 +0300

    functions in ruby

--- App.rb
+++ App.rb
@@ -14,5 +14,10 @@ height = 23.5
 
 puts "my name  is #{name} and am #{age} years old with a height of #{height} cm "
 
-# decllare a function
+# declare a function
+
+def hello(name)
+  puts "hello #{name}"
+end
+hello(name)
 
