2ef3ef43ceff498c6579506f7355d226c5ccc0c9
Kmike <44137594+kmike-wachira@users.noreply.github.com>
2023-04-13 08:49:05 +0300

    classes and objects

--- App.rb
+++ App.rb
@@ -12,13 +12,25 @@ name = "Mike"
 age = 24
 height = 23.5
 
-puts "my name  is #{name} and am #{age} years old with a height of #{height} cm "
+# puts "my name  is #{name} and am #{age} years old with a height of #{height} cm "
 
 # declare a function
 
 def hello(name)
-  puts "hello #{name}"
+#   puts "hello #{name}"
 end
 # call the function
 hello(name)
+class Person
+    attr_accessor :name ,:age
+    def initialize(name,age)
+        @name = name
+        @age = age
+    end
+    def sayName()
+        puts "hello #{name} and am #{age} years old"
+    end
+end
 
+person1=Person.new( "Mike kariuki",23)
+person1.sayName()
